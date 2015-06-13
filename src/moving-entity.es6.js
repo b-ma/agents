f084e696f77fb5dc77a1af75edceaae3a49b5938
b-ma <benjamin.matuszewski@gmail.com>
2015-06-13 11:24:22 +0200

    merged vehicle into moving-entity

--- src/moving-entity.es6.js
+++ src/moving-entity.es6.js
@@ -1,12 +1,17 @@
 var vector = require('vector');
 var BaseGameEntity = require('./base-game-entity');
+var SteeringBehaviors = require('./steering-behaviors');
 
 var entityType = 0;
 
 class MovingEntity extends BaseGameEntity {
 
-  constructor(position, boundingRadius, velocity, maxSpeed, heading, mass, maxTurnRate, maxForce) {
+  constructor(world, position, boundingRadius, velocity, maxSpeed, heading, mass, maxTurnRate, maxForce) {
     super(entityType, position, boundingRadius);
+
+    // call some kind of super
+    this.world = world;
+    this.steerings = new SteeringBehaviors(this);
 
     this.velocity = velocity;
     // normalized vector pointing in the direction the entity is moving
@@ -25,6 +30,29 @@ class MovingEntity extends BaseGameEntity {
 
   speed() { return this.velocity.magnitude(); }
   speedSqrt() { return this.velocity.magnitudeSqrt(); }
+
+  update(dt) {
+    // define all the forces applied on the agent
+    var steeringForces = this.steerings.calculate();
+    // console.log(steeringForces);
+    // newton
+    this.acceleration = Vector.divide(steeringForces, this.mass);
+    // reverse fourier integration - could be a service
+    this.velocity.add(Vector.multiply(this.acceleration, dt));
+    // vehicle cannot go beyong max speed
+    this.velocity.truncate(this.maxSpeed);
+    // update location
+    this.position.add(Vector.multiply(this.velocity, dt));
+
+    // prevent stupid display behavior (turning when stopped)
+    // console.log(this.velocity.magnitudeSqrt());
+    if (this.velocity.magnitudeSqrt() > 0.00001) {
+      this.heading = Vector.normalize(this.velocity);
+      this.side = Vector.orthogonal(this.heading);
+    }
+  }
+
+  render(ctx, buffers/*, worldSize */) {}
 }
 
 module.exports = MovingEntity
